b32c7fd6936271a68494324a30a9336cac1d32dc
caisr <383911973@qq.com>
2019-03-27 22:22:22 +0800

    优化MCascader组件不设置v-model的处理

--- src/components/cascader/index.vue
+++ src/components/cascader/index.vue
@@ -14,7 +14,7 @@
         :source="source"
         v-if="visiblePopover"
         @change="onChange"
-        :selected="selected"
+        :selected="selected || copySelected"
         @update:selected="onUpdateSelected"
       ></m-cascader-item>
     </div>
@@ -46,12 +46,11 @@ export default class MCascader extends Vue {
   visiblePopover: boolean = false;
   placeholder: string = "请选择";
   selectedContentArr: any[] = [];
-  mounted() {
-    this.checkSelected();
-  }
+  copySelected: any[] = [];
   // methods
   onUpdateSelected(selected: any[]) {
     this.$emit("select", selected);
+    this.copySelected = selected;
   }
   onChange(sourceItem: SourceItem, level: number) {
     const contentArr = JSON.parse(JSON.stringify(this.selectedContentArr));
@@ -59,13 +58,6 @@ export default class MCascader extends Vue {
     contentArr.splice(level + 1);
     this.selectedContentArr = contentArr;
     this.$emit("change", sourceItem);
-  }
-  checkSelected(): void {
-    if (!(this.selected instanceof Array)) {
-      console.error(
-        "请在m-cascader组件上使用v-model指令，并给v-model指令一个数组类型的值"
-      );
-    }
   }
   // computed
   get selectedContent(): string {
@@ -92,8 +84,9 @@ export default class MCascader extends Vue {
     display: flex;
     align-items: center;
     justify-content: space-between;
+    width: 200px;
     > p {
-      width: 200px;
+      flex: 1;
       margin: 0;
       overflow: hidden;
       text-overflow: ellipsis;
