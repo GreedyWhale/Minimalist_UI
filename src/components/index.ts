1f2707052d6cfa5e9ff4cdacca95ca8f81c368f7
caisr <383911973@qq.com>
2019-04-23 16:49:00 +0800

    导出所有组件 & 更新版本号

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,35 @@
 export { default as MButton } from "@/components/button/index.vue";
 export { default as MIcon } from "@/components/icon/index.vue";
+export { default as MButtonGroup } from "@/components/button-group/index.vue";
+export { default as MCascader } from "@/components/cascader/index.vue";
+export { default as MCollapse } from "@/components/collapse/index.vue";
+export { default as MCollapseItem } from "@/components/collapse/item/index.vue";
+export { default as MDatePicker } from "@/components/date-picker/index.vue";
+export { default as MDialog } from "@/components/dialog/index.vue";
+export { default as MCol } from "@/components/grid/col/index.vue";
+export { default as MRow } from "@/components/grid/row/index.vue";
+export { default as MInput } from "@/components/input/index.vue";
+export { default as MLayout } from "@/components/layout/index.vue";
+export { default as MLayoutAside } from "@/components/layout/aside/index.vue";
+export { default as MLayoutFooter } from "@/components/layout/footer/index.vue";
+export { default as MLayoutHeader } from "@/components/layout/header/index.vue";
+export { default as MLayoutMain } from "@/components/layout/main/index.vue";
+export { default as MMenu } from "@/components/menu/index.vue";
+export { default as MMenuItem } from "@/components/menu/item/index.vue";
+export { default as MMenuSubItem } from "@/components/menu/sub/index.vue";
+export { default as MPagination } from "@/components/pagination/index.vue";
+export { default as MPopover } from "@/components/popover/index.vue";
+export { default as MSticky } from "@/components/sticky/index.vue";
+export { default as MSwiper } from "@/components/swiper/index.vue";
+export { default as MSwiperItem } from "@/components/swiper/item/index.vue";
+export { default as MTable } from "@/components/table/index.vue";
+export { default as MTabs } from "@/components/tabs/index.vue";
+export { default as MTabsBody } from "@/components/tabs/body/index.vue";
+export { default as MTabsHead} from "@/components/tabs/head/index.vue";
+export { default as MTabsItem } from "@/components/tabs/item/index.vue";
+export { default as MTabsPane } from "@/components/tabs/pane/index.vue";
+export { default as MToast } from "@/components/toast/index.vue";
+export { default as MUpload } from "@/components/upload/index.vue";
+export { default as MToastPlugin } from "@/plugin/toast.ts";
+
+
